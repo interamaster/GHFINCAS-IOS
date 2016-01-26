c24c7a180b539ff85904e92f0eff1c172b541711
yo <interamaster@gmail.com>
2016-01-26 19:53:57 +0100

    3

--- GHFINCAS/FirstViewController.m
+++ GHFINCAS/FirstViewController.m
@@ -7,12 +7,44 @@
 //
 
 #import "FirstViewController.h"
+//para el alertView que marque el telfono
+
+#import "ILAlertView.h"
+
+
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *telefono24h;
 
 @end
 
 @implementation FirstViewController
+- (IBAction)pulsado_telefono24h:(id)sender {
+    
+    //NSLog(@"pulsado en 24h");
+    
+    NSString *phNo = @"673787175";
+    NSURL *phoneUrl = [NSURL URLWithString:[NSString stringWithFormat:@"telprompt:%@",phNo]];
+    
+     if([[UIApplication sharedApplication] canOpenURL:phoneUrl]){
+        
+        [ILAlertView showWithTitle:@"LLAMAR GHFINCAS!!"
+                           message:[@"Quieres llamar a " stringByAppendingString:phNo]
+                  closeButtonTitle:@"No"
+                 secondButtonTitle:@"SI"
+                tappedSecondButton:^{
+                    //aqui ahora llamamos al telefno
+                    [[UIApplication sharedApplication] openURL:phoneUrl];
+                    
+                }];
+        
+        
+     };
+}
+- (IBAction)pulsado_info:(id)sender {
+     NSLog(@"pulsado info");
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
