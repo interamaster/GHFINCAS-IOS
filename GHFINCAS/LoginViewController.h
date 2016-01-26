f4c5f981c4ef0071e155d094bdcac921bd3524b9
yo <interamaster@gmail.com>
2016-01-26 23:33:37 +0100

    4

--- GHFINCAS/LoginViewController.h
+++ GHFINCAS/LoginViewController.h
@@ -8,10 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+#import "UCZProgressView.h"
+
+
+
 @interface LoginViewController : UIViewController <UITextFieldDelegate>
  
 @property (weak, nonatomic) IBOutlet UITextField *login;
 @property (weak, nonatomic) IBOutlet UISwitch *autolgin;
+@property (nonatomic)  UCZProgressView *progressView;
+
+@property (nonatomic)NSTimer* timerProgressBar;
+
  
 @property (weak, nonatomic) IBOutlet UITextField *password;
 - (IBAction)login_pulsado:(id)sender;
--- GHFINCAS/LoginViewController.m
+++ GHFINCAS/LoginViewController.m
@@ -10,13 +10,20 @@
 
 @interface LoginViewController ()
 
+
+
 @end
 
 @implementation LoginViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -78,10 +85,61 @@
 -(void)LoginSuces{
     
     
-    [self performSegueWithIdentifier:@"login_success" sender:self];
+    //par el progressView
+    
+    
+    self.progressView = [[UCZProgressView alloc] initWithFrame:self.view.bounds];
+    self.progressView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.progressView.blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    self.progressView.tintColor = [UIColor greenColor];
+    self.progressView.showsText = YES;
+    self.progressView.radius = 100.0;
+    [self.view addSubview:self.progressView];
+    
+    NSDictionary *views = NSDictionaryOfVariableBindings(_progressView);
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[_progressView]-0-|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[_progressView]-0-|" options:0 metrics:nil views:views]];
+    
+    
+    self.progressView.progress=0.0;
+    
+    self.timerProgressBar=[NSTimer scheduledTimerWithTimeInterval:0.1
+                                     target:self
+                                   selector:@selector(increaseProgressBar)
+                                   userInfo:nil
+                                    repeats:YES];
+    
+    //[self performSegueWithIdentifier:@"login_success" sender:self];
     
 }
 
+-(void)increaseProgressBar {
+    
+    self.progressView.progress=self.progressView.progress+0.1;
+    
+    NSLog(@"increase  progress bar!");
+
+    
+    if (self.progressView.progress >=1){
+        
+        //llego al 100%
+        
+        NSLog(@"100 progress bar!");
+        
+        //paramos el timer:
+        
+        [self.timerProgressBar invalidate];
+        
+        
+        
+        //pasamos al FristVCEmpresa)
+        
+        [self performSegueWithIdentifier:@"login_success" sender:self];
+        
+    }
+        
+        
+}
 
 
 - (IBAction)sign_in_pulsado:(id)sender {
