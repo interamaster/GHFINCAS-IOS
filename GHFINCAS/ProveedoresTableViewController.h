0eb137e080b3c9e027802fb7389dd8b0679eb0d6
yo <interamaster@gmail.com>
2016-02-02 18:29:40 +0100

    creado tableview en proveedores empezando diseño

---
+++ GHFINCAS/ProveedoresTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProveedoresTableViewController.h
+//  GHFINCAS
+//
+//  Created by Jose Ramon MBP on 02/02/16.
+//  Copyright (c) 2016 JRDV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProveedoresTableViewController : UITableViewController
+
+@end
